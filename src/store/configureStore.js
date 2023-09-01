f4b3d704680b914b26375513cde0bf9d95874bca
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-01 17:20:27 +0530

    store added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,16 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux'
-import rootReducer from '../reducers'
-import createSagaMiddleware from 'redux-saga'
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
+
+// export default function configureStore() {
+//   const sagaMiddleware = createSagaMiddleware();
+//   const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+//   return {
+//     store,
+//     runSaga: sagaMiddleware.run,
+//   };
+// }
+
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware()
