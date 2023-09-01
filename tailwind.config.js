2853c0c8370db283b9f67ce1c15844447b03359f
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-01 17:23:18 +0530

    packages updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // animation: {
+      //   'spin-slow': 'spin 3s linear infinite',
+      // },
+      // keyframes: {
+      //   wiggle: {
+      //     '0%, 100%': { transform: 'rotate(-3deg)' },
+      //     '50%': { transform: 'rotate(3deg)' },
+      //   }
+      // }
+    },
   },
   plugins: [],
 }
