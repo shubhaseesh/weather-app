18a3301a7431afc4e6ebc77fa77599180c7d07bc
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-01 17:21:44 +0530

    small fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./containers/App.jsx";
 import "./index.css";
+import rootSaga from "./sagas/index.js";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore.js";
 
-const store = configureStore();
+const store = configureStore()
+store.runSaga(rootSaga)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
