f2ac4f8ad90c2153a659575972daa44c61951c5b
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-01 17:19:11 +0530

    weather component added

---
+++ src/components/WeatherComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WeatherComponent = () => {
+  return (
+    <div className="m-24">
+      <div>Weather Data</div>
+    </div>
+  );
+};
+
+export default WeatherComponent;
