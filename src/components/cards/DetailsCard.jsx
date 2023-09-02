c07296529be2608ff5091a38f20d1b9b0176d1bb
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-02 21:32:30 +0530

    border added

--- src/components/cards/DetailsCard.jsx
+++ src/components/cards/DetailsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const DetailsCard = ({ label, value }) => (
-  <div className="inline-flex items-center bg-gray-100 p-4">
+  <div className="inline-flex items-center border bg-gray-100 p-4">
     <p className="font-semibold mr-2">{label}:</p>
     <div>{value}</div>
   </div>
