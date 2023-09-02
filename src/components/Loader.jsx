7236bd731abb4934f9763253ed477482cfcb7be7
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-02 21:18:00 +0530

    loader color changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div
-      className="inline-block w-8 h-8 rounded-full border-4 border-current border-r-transparent align-[0.125em] animate-[spin_1.5s_linear_infinite] text-black"
+      className="inline-block w-8 h-8 rounded-full border-4 border-current border-r-transparent align-[0.125em] animate-[spin_1.5s_linear_infinite] text-gray-700"
       role="status"
     >
       <span className="absolute m-0 h-0 w-0 overflow-hidden whitespace-nowrap border-0 p-0 clip-[rect(0,0,0,0)]"></span>
