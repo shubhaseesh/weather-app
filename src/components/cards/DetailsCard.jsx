19836483523baba9a7a4cc0099fe4cac3ee88874
shubhaseesh <shubhaseesh09@gmail.com>
2023-09-03 16:01:52 +0530

    font styling added

--- src/components/cards/DetailsCard.jsx
+++ src/components/cards/DetailsCard.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 
 const DetailsCard = ({ label, value }) => (
   <div className="inline-flex items-center border bg-gray-100 p-4">
-    <p className="font-semibold mr-2">{label}:</p>
-    <div>{value}</div>
+    <p className="text-red-700 font-bold hover:bold hover:text-lg mr-2">{label} -</p>
+    <p className="font-bold">{value}</p>
   </div>
 );
 
